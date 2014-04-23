4d6fd6f9eb0ab55fdf2d3e41aa7edb866e33fe84
ohhdemgirls <ohhdemgirls@ymail.com>
2014-04-23 21:03:39 +0100

    http --> https

--- get_all_photos.rb
+++ get_all_photos.rb
@@ -16,14 +16,14 @@ baseDirectory = Dir.new(Dir.pwd)
 Dir.mkdir("images") unless File.exists?("images")
 
 puts "Loading Flickr page..."
-mainDoc = Nokogiri::HTML(open("http://www.flickr.com/photos/#{username}"))
+mainDoc = Nokogiri::HTML(open("https://www.flickr.com/photos/#{username}"))
 mainDoc.xpath("//div[@data-page-count]/@data-page-count").each do |i|
 	pageCount = i.value.to_i
 end
 
 puts "Loading photo IDs from #{pageCount} page(s)..."
 1.step(pageCount,1) do |page|
-	doc = Nokogiri::HTML(open("http://www.flickr.com/photos/#{username}/page#{page}/"))
+	doc = Nokogiri::HTML(open("https://www.flickr.com/photos/#{username}/page#{page}/"))
 	doc.xpath("//div[@class='title']/a[@data-track='photo-click']/@href").each do |link|
 		photoIdStore = photoId.match(link)
 		photoIds.push(photoIdStore)
@@ -32,7 +32,7 @@ end
 
 puts "Downloading #{photoIds.length} photos..."
 photoIds.each do |myPhotoId|
-	imgDoc = Nokogiri::HTML(open("http://www.flickr.com/photos/#{username}/#{myPhotoId}/sizes/o/in/photostream/"))
+	imgDoc = Nokogiri::HTML(open("https://www.flickr.com/photos/#{username}/#{myPhotoId}/sizes/o/in/photostream/"))
 	imgDoc.xpath("//div[@id='allsizes-photo']/img/@src").each do |link|
 		open(link) do |full_size_image|
 		  	File.open("images/#{myPhotoId}.jpg","wb") do |file|
